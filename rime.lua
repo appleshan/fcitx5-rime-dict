301960b1522d7d2fdaa2e7f8a4cb5b84eb3c74a6
appleshan <apple.shan@gmail.com>
2022-07-13 14:19:45 +0800

    完善「长词优先」

--- rime.lua
+++ rime.lua
@@ -134,34 +134,38 @@ function select_character(key, env)
 end
 -------------------------------------------------------------
 -- 长词优先（提升「西安」「提案」「图案」「饥饿」等词汇的优先级）
--- https://github.com/tumuyan/rime-melt
+-- 感谢&参考于： https://github.com/tumuyan/rime-melt
 -- 修改：不提升英文和中英混输的
 function long_word_filter(input)
+        -- 目前的效果：将 3 个词插入到第 3、4、5 个候选项
+    local count = 3 -- 提升 count 个词语
+    local idx = 3   -- 插入到第 idx 位
+
+
     local l = {}
-    local length = 0 -- 记录第一个候选词的长度，提前的候选词至少要比第一个候选词长
-    -- local s1 = 0 -- 记录筛选了多少个英语词条(只提升3个词的权重，并且对comment长度过长的候选进行过滤)
-    local s2 = 0 -- 记录筛选了多少个汉语词条(只提升3个词的权重)
+    local firstWordLength = 0 -- 记录第一个候选词的长度，提前的候选词至少要比第一个候选词长
+    -- local s1 = 0 -- 记录筛选了多少个英语词条(只提升 count 个词的权重，并且对comment长度过长的候选进行过滤)
+    local s2 = 0 -- 记录筛选了多少个汉语词条(只提升 count 个词的权重)
 
-    -- 目前的效果：将 2 个词插入到第 2、3 位
-    local count = 2 -- 提升 count 个词语
-    local idx = 0 -- (idx < N) 即从第 N+1 个位置开始插入
-
+    local i = 1
     for cand in input:iter() do
         leng = utf8.len(cand.text)
-        if (length < 1 or idx < 1) then
-            idx = idx + 1
-            length = leng
+        if (firstWordLength < 1 or i < idx) then
+            i = i + 1
+            firstWordLength = leng
             yield(cand)
                 -- 不知道这两行是干嘛用的，似乎注释掉也没有影响。
                 -- elseif #table > 30 then
                 --     table.insert(l, cand)
-                -- 英文和中英混输的，取消提升
-                -- elseif ((leng > length) and (s1 < 2)) and (string.find(cand.text, "^[%w%p%s]+$")) then
+                -- 注释掉了英文的
+                -- elseif ((leng > firstWordLength) and (s1 < 2)) and (string.find(cand.text, "^[%w%p%s]+$")) then
                 --     s1 = s1 + 1
                 --     if (string.len(cand.text) / string.len(cand.comment) > 1.5) then
                 --         yield(cand)
                 --     end
-        elseif ((leng > length) and (s2 < count)) and (string.find(cand.text, "[%w%p%s]+") == nil) then
+                -- 换了个正则，否则中英混输的也会被提升
+                -- elseif ((leng > firstWordLength) and (s2 < count)) and (string.find(cand.text, "^[%w%p%s]+$")==nil) then
+        elseif ((leng > firstWordLength) and (s2 < count)) and (string.find(cand.text, "[%w%p%s]+") == nil) then
             yield(cand)
             s2 = s2 + 1
         else
