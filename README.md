ac7513eea20e5d5c705debc2bffe365bd0faf820
appleshan <apple.shan@gmail.com>
2022-07-13 11:01:35 +0800

    feat: 以词定字

--- README.md
+++ README.md
@@ -4,19 +4,20 @@
 
 ## 基本套路
 
--   简体 全拼
+-   简体全拼
     - 小狼毫 Weasel 0.14.3
 -   「[袖珍简化字方案](https://github.com/rime/rime-pinyin-simp)」作为基础
     - [简繁切换](https://github.com/rime/home/issues/388#issuecomment-504572224)
     - [动态日期、时间、星期](https://github.com/KyleBing/rime-wubi86-jidian)
+    - [以词定字](https://github.com/BlindingDark/rime-lua-select-character)
     - 所有标点符号直接上屏，「/」模式改为「v」模式，「/」直接上屏
     - 将「[Emoji](https://github.com/rime/rime-emoji)」改为词语与符号映射
     - 增加了许多拼音纠错
 -   融合「[easy_en](https://github.com/BlindingDark/rime-easy-en)」英文输入方案
 -   纯简体字表、词库（这样在用户词典中也是简体了）
     - 字表：[《通用规范汉字表》的 8105 字字表](https://github.com/iDvel/The-Table-of-General-Standard-Chinese-Characters)
-    - 词库：「[华宇野风系统词库](http://bbs.pinyin.thunisoft.com/forum.php?mod=viewthread&tid=30049)」 +「[清华大学开源词库](https://github.com/thunlp/THUOCL)」
-    - 「[简化字八股文语言模型](https://github.com/lotem/rime-octagram-data/tree/hans)」
+    - 词库：「[华宇野风系统词库](http://bbs.pinyin.thunisoft.com/forum.php?mod=viewthread&tid=30049)」+「[清华大学开源词库](https://github.com/thunlp/THUOCL)」
+    - [简化字八股文语言模型](https://github.com/lotem/rime-octagram-data/tree/hans)
 -   长期持续修订遇到的异形词、错别字、错误注音
 
 <br>
--- rime.lua
+++ rime.lua
@@ -1,9 +1,11 @@
+-- 日期时间
 function date_translator(input, seg)
     -- 日期
     if (input == "rq") then
         --- Candidate(type, start, end, text, comment)
         yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y.%m.%d"), ""))
         -- yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%Y 年 %m 月 %d 日"), "")) -- 加些空格
         -- yield(Candidate("date", seg.start, seg._end, os.date("%m-%d"), ""))
@@ -15,12 +17,9 @@ function date_translator(input, seg)
         --- Candidate(type, start, end, text, comment)
         yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), ""))
         yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
-        yield(Candidate("time", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), ""))
     end
 
-    -- 星期
-    -- @JiandanDream
-    -- https://github.com/KyleBing/rime-wubi86-jidian/issues/54
+    -- 星期  @JiandanDream  https://github.com/KyleBing/rime-wubi86-jidian/issues/54
     if (input == "xq") then
         local weakTab = {'日', '一', '二', '三', '四', '五', '六'}
         yield(Candidate("week", seg.start, seg._end, "周"..weakTab[tonumber(os.date("%w")+1)], ""))
@@ -29,11 +28,76 @@ function date_translator(input, seg)
 
     -- ISO 8601/RFC 3339 的时间格式 （固定东八区）（示例 2022-01-07T20:42:51+08:00）
     if (input == "dt") then
-      yield(Candidate("datetime", seg.start, seg._end, os.date("%Y-%m-%dT%H:%M:%S+08:00"), ""))
+        yield(Candidate("datetime", seg.start, seg._end, os.date("%Y-%m-%dT%H:%M:%S+08:00"), ""))
+        yield(Candidate("time", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), ""))
     end
 
     -- 时间戳（十位数，到秒，示例 1650861664）
     if (input == "ts") then
-      yield(Candidate("datetime", seg.start, seg._end, os.time(), ""))
+        yield(Candidate("datetime", seg.start, seg._end, os.time(), ""))
     end
+end
+
+
+-- 以词定字  https://github.com/BlindingDark/rime-lua-select-character
+-- 设定的快捷键请看 select_character() 方法下面的注释
+local function utf8_sub(s, i, j)
+    i = i or 1
+    j = j or -1
+
+    if i < 1 or j < 1 then
+         local n = utf8.len(s)
+         if not n then return nil end
+         if i < 0 then i = n + 1 + i end
+         if j < 0 then j = n + 1 + j end
+         if i < 0 then i = 1 elseif i > n then i = n end
+         if j < 0 then j = 1 elseif j > n then j = n end
+    end
+
+    if j < i then return "" end
+
+    i = utf8.offset(s, i)
+    j = utf8.offset(s, j + 1)
+
+    if i and j then
+         return s:sub(i, j - 1)
+    elseif i then
+         return s:sub(i)
+    else
+         return ""
+    end
+end
+
+local function first_character(s)
+    return utf8_sub(s, 1, 1)
+end
+
+local function last_character(s)
+    return utf8_sub(s, -1, -1)
+end
+
+function select_character(key, env)
+    local engine = env.engine
+    local context = engine.context
+    local commit_text = context:get_commit_text()
+    local config = engine.schema.config
+
+    local first_key = config:get_string('key_binder/select_first_character') or 'bracketleft'
+    local last_key = config:get_string('key_binder/select_last_character') or 'bracketright'
+
+    if (key:repr() == first_key and commit_text ~= "") then
+         engine:commit_text(first_character(commit_text))
+         context:clear()
+
+         return 1 -- kAccepted
+    end
+
+    if (key:repr() == last_key and commit_text ~= "") then
+         engine:commit_text(last_character(commit_text))
+         context:clear()
+
+         return 1 -- kAccepted
+    end
+
+    return 2 -- kNoop
 end
